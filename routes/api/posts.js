b71b53f6cfd36a8aa61e17b25aec4bad2ecbeffb
dreamdeveloper002 <bkosemani1@gmail.com>
2021-06-06 23:09:41 +0100

    post route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,37 +1,37 @@
-// const express = require('express');
-// const app = express();
+const express = require('express');
+const app = express();
 
-// const router = express.Router();
-// const bodyParser = require('body-parser');
-// const Post = require('../../schemas/PostUser');
+const router = express.Router();
+const bodyParser = require('body-parser');
+const Post = require('../../schemas/PostUser');
 
 
-// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-// router.get("/", (req, res, next) => {
-//     res.status(200).render("login")
-// });
+router.get("/", (req, res, next) => {
+    res.status(200).render("login")
+});
 
-// router.post("/", async (req, res, next) => {
-//   if(!req.body.content) {
-//      console.log("Content wasn't send with the request");
-//      return res.sendStatus(400)
-//   };
+router.post("/", async (req, res, next) => {
+  if(!req.body.content) {
+     console.log("Content wasn't send with the request");
+     return res.sendStatus(400)
+  };
 
-//   var postData = {
-//     content: req.body.content,
-//     postedBy: req.session.user
-//   };
+  var postData = {
+    content: req.body.content,
+    postedBy: req.session.user
+  };
 
-//   Post.create(postData)
-//   .then(newPost =>{
-//     return res.status(201).send(newPost)
-//   }).catch(error => {
-//       console.log(error);
-//       res.sendStatus(400)
-//   });
+  Post.create(postData)
+  .then(newPost =>{
+    return res.status(201).send(newPost)
+  }).catch(error => {
+      console.log(error);
+      res.sendStatus(400)
+  });
 
-// });
+});
 
 
 
