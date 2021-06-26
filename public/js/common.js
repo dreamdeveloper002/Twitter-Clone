b3d38ff9bc6a061a937f78451ef849aafe8733aa
dreamdeveloper002 <bkosemani1@gmail.com>
2021-06-26 23:00:17 +0100

    home page

--- public/js/common.js
+++ public/js/common.js
@@ -3,7 +3,7 @@ $("#postTextarea").keyup(event => {
   var value = textbox.val().trim();
   
   var submitButton = $("#submitPostButton");
- 
+
   if(submitButton.length == 0) return alert("No submit button found");
 
   if (value == "") {
@@ -12,51 +12,64 @@ $("#postTextarea").keyup(event => {
   }
 
   submitButton.prop("disabled", false);
-});
+})
 
-
-$("#submitPostButton").click(()=>{
+$("#submitPostButton").click(() => {
   var button = $(event.target);
   var textbox = $("#postTextarea");
- 
 
   var data = {
-     content: textbox.val()
-  };
-
+      content: textbox.val()
+  }
 
   $.post("/api/posts", data, postData => {
-       var html = createPostHtml(postData);
-       $('.postsContainer').prepend(html);
-       textbox.val('');
-       button.prop("disabled", true);
-  });
+      
+      var html = createPostHtml(postData);
+      $(".postsContainer").prepend(html);
+      textbox.val("");
+      button.prop("disabled", true);
+  })
+})
 
-});
+function createPostHtml(postData) {
+  
+  var postedBy = postData.postedBy;
+  var displayName = postedBy.firstName + " " + postedBy.lastName;
+  var timestamp = postData.createdAt;
 
+  return `<div class='post'>
 
-function createPostHtml(postData){
-
-    var postedBy = postData.postedBy;
-    var displayName = postedBy.firstName + ' ' + postedBy.lastName;
-    var timestamp = postData.createdAt
-    return `<div class='post'>
-       <div class='mainContentContainer'>
-          <div class='userImageContainer'>
-             <img src='${postedBy.profilePic}'>
-          </div>
-          <div class='postContentContainer'>
-          <div class='header'>
-           <a href='/profile/${postedBy.username}'>${displayName}</a>
-           <span class='username'>${postedBy.username}</span>
-           <span class='date'>${timestamp}</span>
-          </div>
-          <div class='postBody'>
-          <span>${postData.content}</span>
-          </div>
-          <div class='postFooter'>
-          </div>
-          </div>
-       </div>
-    </div>`;
+              <div class='mainContentContainer'>
+                  <div class='userImageContainer'>
+                      <img src='${postedBy.profilePic}'>
+                  </div>
+                  <div class='postContentContainer'>
+                      <div class='header'>
+                          <a href='/profile/${postedBy.username}' class='displayName'>${displayName}</a>
+                          <span class='username'>@${postedBy.username}</span>
+                          <span class='date'>${timestamp}</span>
+                      </div>
+                      <div class='postBody'>
+                          <span>${postData.content}</span>
+                      </div>
+                      <div class='postFooter'>
+                          <div class='postButtonContainer'>
+                              <button>
+                                  <i class='far fa-comment'></i>
+                              </button>
+                          </div>
+                          <div class='postButtonContainer'>
+                              <button>
+                                  <i class='fas fa-retweet'></i>
+                              </button>
+                          </div>
+                          <div class='postButtonContainer'>
+                              <button>
+                                  <i class='far fa-heart'></i>
+                              </button>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>`;
 }
