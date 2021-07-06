47b38203691410d07619142b9a07f7fcecc20698
dreamdeveloper002 <bkosemani1@gmail.com>
2021-07-06 22:47:44 +0100

    fix bug

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,52 +1,52 @@
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
 
-const router = express.Router();
-const bodyParser = require('body-parser');
-const Post = require('../../schemas/PostUser');
-const User = require('../../schemas/UserSchema')
-
-
-app.use(bodyParser.urlencoded({ extended: false }));
-
-router.get("/", (req, res, next) => {
-    res.status(200).render("login")
-});
-
-router.get("/", (req, res, next) => {
-  Post.find()
-  .then( results => res.status(200).send(results))
-  .catch(error => {
-    console.log(error);
-    res.sendStatus(400);
-});
-
-});
-
-router.post("/", async(req, res, next) => {
-  if(!req.body.content) {
-     console.log("Content wasn't send with the request");
-     return res.sendStatus(400)
-  };
-
-  var postData = {
-    content: req.body.content,
-    postedBy: req.session.user
-  };
-
-  Post.create(postData)
-  .then(async newPost =>{
-    newPost = await User.populate(newPost, { path: "postedBy"})
-    res.status(201).send(newPost)
-  })
-  .catch(error => {
-      console.log(error);
-      res.sendStatus(400)
-  });
-
-});
+// const router = express.Router();
+// const bodyParser = require('body-parser');
+// const Post = require('../../schemas/PostUser');
+// const User = require('../../schemas/UserSchema')
 
 
+// app.use(bodyParser.urlencoded({ extended: false }));
+
+// router.get("/", (req, res, next) => {
+//     res.status(200).render("login")
+// });
+
+// router.get("/", (req, res, next) => {
+//   Post.find()
+//   .then( results => res.status(200).send(results))
+//   .catch(error => {
+//     console.log(error);
+//     res.sendStatus(400);
+// });
+
+// });
+
+// router.post("/", async(req, res, next) => {
+//   if(!req.body.content) {
+//      console.log("Content wasn't send with the request");
+//      return res.sendStatus(400)
+//   };
+
+//   var postData = {
+//     content: req.body.content,
+//     postedBy: req.session.user
+//   };
+
+//   Post.create(postData)
+//   .then(async newPost =>{
+//     newPost = await User.populate(newPost, { path: "postedBy"})
+//     res.status(201).send(newPost)
+//   })
+//   .catch(error => {
+//       console.log(error);
+//       res.sendStatus(400)
+//   });
+
+// });
 
 
-module.exports = router;
+
+
+// module.exports = router;
