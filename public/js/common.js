72769922d507bb2c80c7659308ef7f10ddef39ed
dreamdeveloper002 <bkosemani1@gmail.com>
2021-08-07 22:53:59 +0100

    retweet button

--- public/js/common.js
+++ public/js/common.js
@@ -59,8 +59,15 @@ $(document).on("click", ".retweetButton", (event) => {
         url: `/api/posts/${postId}/retweet`,
         type: "POST",
         success: (postData) => {
-        
-            console.log(postData);
+           
+            button.find("span").text(postData.retweetUsers.length || "");
+
+            if(postData.retweetUsers.includes(userLoggedIn._id)) {
+                 button.addClass("active");
+            } else {
+                button.removeClass("active");
+            }
+            
         }
     });
 });
@@ -111,6 +118,7 @@ function createPostHtml(postData) {
                           <div class='postButtonContainer green'>
                               <button class='retweetButton'>
                                   <i class='fas fa-retweet'></i>
+                                  <span>${ postData.retweetUsers.length || ""}</span>
                               </button>
                           </div>
                           <div class='postButtonContainer red'>
