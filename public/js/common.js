44da0f7f0908861db85a2fd696bac3d0c55e4dde
dreamdeveloper002 <bkosemani1@gmail.com>
2021-09-02 23:30:51 +0100

    bug fix

--- public/js/common.js
+++ public/js/common.js
@@ -136,134 +136,134 @@ function getPostIdFromElement(element) {
 
 
 
-function createPostHtml(postData, largefont = false) {
+// function createPostHtml(postData, largefont = false) {
   
-if(postData == null ) return alert("Post object is null");
+// if(postData == null ) return alert("Post object is null");
 
-var isRetweet = postData.retweetData !== undefined;
-var retweetedBy = isRetweet ? postData.postedBy.username : null;
-var postData = isRetweet ? postData.retweetData : postData;
+// var isRetweet = postData.retweetData !== undefined;
+// var retweetedBy = isRetweet ? postData.postedBy.username : null;
+// var postData = isRetweet ? postData.retweetData : postData;
 
 
     
-var postedBy = postData.postedBy;
-var displayName = postedBy.firstName + " " + postedBy.lastName;
-var timestamp = timeDifference(new Date(), new Date(postData.createdAt));
-var likeButtonActiveClass = postData.likes.includes(userLoggedIn._id) ? "active" : "";
-var retweetButtonActiveClass = postData.retweetUsers.includes(userLoggedIn._id) ? "active" : "";
-var largeFontClass = largefont ? "largeFont" : "";
+// var postedBy = postData.postedBy;
+// var displayName = postedBy.firstName + " " + postedBy.lastName;
+// var timestamp = timeDifference(new Date(), new Date(postData.createdAt));
+// var likeButtonActiveClass = postData.likes.includes(userLoggedIn._id) ? "active" : "";
+// var retweetButtonActiveClass = postData.retweetUsers.includes(userLoggedIn._id) ? "active" : "";
+// var largeFontClass = largefont ? "largeFont" : "";
 
-  var retweetText = '';
+//   var retweetText = '';
 
-  if(isRetweet) {
-      retweetText = `<span>
-      <i class='fas fa-retweet'></i> 
-      Retweeted by <a href='/profile/${retweetedBy}'>@${retweetedBy}</a> 
-      </span>`
-  };
+//   if(isRetweet) {
+//       retweetText = `<span>
+//       <i class='fas fa-retweet'></i> 
+//       Retweeted by <a href='/profile/${retweetedBy}'>@${retweetedBy}</a> 
+//       </span>`
+//   };
 
-  var replyFlag = "";
+//   var replyFlag = "";
 
-  if(postData.replyTo && postData.replyTo._id) {
-       if(!postData.replyTo._id) {
-             return alert("Reply to is not populated");
-       } else if(!postData.replyTo.postedBy._id) {
-             return alert("Post by is not populated");
-       }
+//   if(postData.replyTo && postData.replyTo._id) {
+//        if(!postData.replyTo._id) {
+//              return alert("Reply to is not populated");
+//        } else if(!postData.replyTo.postedBy._id) {
+//              return alert("Post by is not populated");
+//        }
 
-       var replyToUsername = postData.replyTo.postedBy.username;
-       replyFlag = `<div class='replyFlag'>
-                        Replying to <a href='/profile/${replyToUsername}'>@${replyToUsername}</a>
-                    </div>`
-  }
+//        var replyToUsername = postData.replyTo.postedBy.username;
+//        replyFlag = `<div class='replyFlag'>
+//                         Replying to <a href='/profile/${replyToUsername}'>@${replyToUsername}</a>
+//                     </div>`
+//   }
 
-  var buttons = "";
+//   var buttons = "";
 
-  if(postData.postedBy._id == userLoggedIn._id) {
-      buttons = `<button data-id="${postData._id}" data-toggle="modal" data-target="#deletePostModal"><i class='fas fa-times'></i></button>`;
-  }
+//   if(postData.postedBy._id == userLoggedIn._id) {
+//       buttons = `<button data-id="${postData._id}" data-toggle="modal" data-target="#deletePostModal"><i class='fas fa-times'></i></button>`;
+//   }
 
-  return `<div class='post ${largeFontClass}' data-id='${postData._id}'>
-              <div class='postActionContainer'>
-                ${retweetText}
-              </div>
+//   return `<div class='post ${largeFontClass}' data-id='${postData._id}'>
+//               <div class='postActionContainer'>
+//                 ${retweetText}
+//               </div>
 
-              <div class='mainContentContainer'>
-                  <div class='userImageContainer'>
-                      <img src='${postedBy.profilePic}'>
-                  </div>
-                  <div class='postContentContainer'>
-                      <div class='header'>
-                          <a href='/profile/${postedBy.username}' class='displayName'>${displayName}</a>
-                          <span class='username'>@${postedBy.username}</span>
-                          <span class='date'>${timestamp}</span>
-                          ${buttons}
-                      </div>
-                      ${replyFlag}
-                      <div class='postBody'>
-                          <span>${postData.content}</span>
-                      </div>
-                      <div class='postFooter'>
-                          <div class='postButtonContainer'>
-                              <button data-toggle='modal' data-target='#replyModal'>
-                                  <i class='far fa-comment'></i>
-                              </button>
-                          </div>
-                          <div class='postButtonContainer green'>
-                              <button class='retweetButton ${retweetButtonActiveClass}'>
-                                  <i class='fas fa-retweet'></i>
-                                  <span>${ postData.retweetUsers.length || ""}</span>
-                              </button>
-                          </div>
-                          <div class='postButtonContainer red'>
-                              <button class='likeButton ${likeButtonActiveClass}'>
-                                  <i class='far fa-heart'></i>
-                                  <span>${ postData.likes.length || ""}</span>
-                              </button>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-          </div>`;
-};
+//               <div class='mainContentContainer'>
+//                   <div class='userImageContainer'>
+//                       <img src='${postedBy.profilePic}'>
+//                   </div>
+//                   <div class='postContentContainer'>
+//                       <div class='header'>
+//                           <a href='/profile/${postedBy.username}' class='displayName'>${displayName}</a>
+//                           <span class='username'>@${postedBy.username}</span>
+//                           <span class='date'>${timestamp}</span>
+//                           ${buttons}
+//                       </div>
+//                       ${replyFlag}
+//                       <div class='postBody'>
+//                           <span>${postData.content}</span>
+//                       </div>
+//                       <div class='postFooter'>
+//                           <div class='postButtonContainer'>
+//                               <button data-toggle='modal' data-target='#replyModal'>
+//                                   <i class='far fa-comment'></i>
+//                               </button>
+//                           </div>
+//                           <div class='postButtonContainer green'>
+//                               <button class='retweetButton ${retweetButtonActiveClass}'>
+//                                   <i class='fas fa-retweet'></i>
+//                                   <span>${ postData.retweetUsers.length || ""}</span>
+//                               </button>
+//                           </div>
+//                           <div class='postButtonContainer red'>
+//                               <button class='likeButton ${likeButtonActiveClass}'>
+//                                   <i class='far fa-heart'></i>
+//                                   <span>${ postData.likes.length || ""}</span>
+//                               </button>
+//                           </div>
+//                       </div>
+//                   </div>
+//               </div>
+//           </div>`;
+// };
 
 
 
-function timeDifference(current, previous) {
+// function timeDifference(current, previous) {
 
-    var msPerMinute = 60 * 1000;
-    var msPerHour = msPerMinute * 60;
-    var msPerDay = msPerHour * 24;
-    var msPerMonth = msPerDay * 30;
-    var msPerYear = msPerDay * 365;
+//     var msPerMinute = 60 * 1000;
+//     var msPerHour = msPerMinute * 60;
+//     var msPerDay = msPerHour * 24;
+//     var msPerMonth = msPerDay * 30;
+//     var msPerYear = msPerDay * 365;
 
-    var elapsed = current - previous;
+//     var elapsed = current - previous;
 
-    if (elapsed < msPerMinute) {
-         if(elapsed/1000 < 30) return "Just now";
-         return Math.round(elapsed/1000) + ' seconds ago';   
-    }
+//     if (elapsed < msPerMinute) {
+//          if(elapsed/1000 < 30) return "Just now";
+//          return Math.round(elapsed/1000) + ' seconds ago';   
+//     }
 
-    else if (elapsed < msPerHour) {
-         return Math.round(elapsed/msPerMinute) + ' minutes ago';   
-    }
+//     else if (elapsed < msPerHour) {
+//          return Math.round(elapsed/msPerMinute) + ' minutes ago';   
+//     }
 
-    else if (elapsed < msPerDay ) {
-         return Math.round(elapsed/msPerHour ) + ' hours ago';   
-    }
+//     else if (elapsed < msPerDay ) {
+//          return Math.round(elapsed/msPerHour ) + ' hours ago';   
+//     }
 
-    else if (elapsed < msPerMonth) {
-        return Math.round(elapsed/msPerDay) + ' days ago';   
-    }
+//     else if (elapsed < msPerMonth) {
+//         return Math.round(elapsed/msPerDay) + ' days ago';   
+//     }
 
-    else if (elapsed < msPerYear) {
-        return Math.round(elapsed/msPerMonth) + ' months ago';   
-    }
+//     else if (elapsed < msPerYear) {
+//         return Math.round(elapsed/msPerMonth) + ' months ago';   
+//     }
 
-    else {
-        return Math.round(elapsed/msPerYear ) + ' years ago';   
-    }
-};
+//     else {
+//         return Math.round(elapsed/msPerYear ) + ' years ago';   
+//     }
+// };
 
 
 
