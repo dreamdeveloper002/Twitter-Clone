d463bf0cb897bb5ad1b0a007807849ddf368aef5
dreamdeveloper002 <bkosemani1@gmail.com>
2021-09-14 23:29:39 +0100

    fix bug

--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -17,65 +17,65 @@ router.get("/", (req, res, next) => {
 });
 
 
-// router.post("/", async (req, res, next) => {
+router.post("/", async (req, res, next) => {
 
-//   var firstName = req.body.firstName.trim();
-//   var lastName = req.body.lastName.trim();
-//   var username = req.body.username.trim();
-//   var email = req.body.email.trim();
-//   var password = req.body.password
+  var firstName = req.body.firstName.trim();
+  var lastName = req.body.lastName.trim();
+  var username = req.body.username.trim();
+  var email = req.body.email.trim();
+  var password = req.body.password
 
-//   const payload = req.body;
+  const payload = req.body;
 
-//   if( firstName && lastName && username && email && password) {
+  if( firstName && lastName && username && email && password) {
       
-//      const user = await User.findOne({ 
-//         $or: [ 
-//           { username: username },
-//           { email: email }
-//         ]
-//       }).catch((error) => {
-//         payload.errorMessage = "Something went wrong.";
-//         res.status(200).render("register", payload);
+     const user = await User.findOne({ 
+        $or: [ 
+          { username: username },
+          { email: email }
+        ]
+      }).catch((error) => {
+        payload.errorMessage = "Something went wrong.";
+        res.status(200).render("register", payload);
 
-//       });
+      });
 
-//       if(user == null ) {
+      if(user == null ) {
 
-//         //User doesn't exit
-//         var data = req.body;
+        //User doesn't exit
+        var data = req.body;
 
-//         data.password = await bcrypt.hash(password, 10);
-//         User.create(data)
-//         .then((user) => {
-//            console.log(user)
-//            req.session.user = user;
-//            return res.redirect('/');
-//         })
+        data.password = await bcrypt.hash(password, 10);
+        User.create(data)
+        .then((user) => {
+           console.log(user)
+           req.session.user = user;
+           return res.redirect('/');
+        })
 
-//       } else {
-//           //User found
-//           if( email == user.email) {
+      } else {
+          //User found
+          if( email == user.email) {
             
-//             payload.errorMessage = "Email already in use.";
-//             console.log(payload.errorMessage)
+            payload.errorMessage = "Email already in use.";
+            console.log(payload.errorMessage)
 
-//           } else {
+          } else {
             
-//             payload.errorMessage = "Username already in use.";
-//           }
+            payload.errorMessage = "Username already in use.";
+          }
 
-//           res.status(200).render("register", payload);
-//       }
+          res.status(200).render("register", payload);
+      }
 
-//   } else {
+  } else {
     
-//     payload.errorMessage = "Make sure each field have a valid value"
-//     res.status(200).render("register", payload);
+    payload.errorMessage = "Make sure each field have a valid value"
+    res.status(200).render("register", payload);
 
-//   }
+  }
   
-// });
+});
 
 
 
